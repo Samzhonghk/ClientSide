70f5d6e755e6ac15564d8a5a026a4747855ebbaa
Samzhonghk <samzhongnz@gmail.com>
2016-09-16 10:48:20 +1200

    A socket chasing project with client side

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ClientSide
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(43, 39);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(135, 20);
+            this.txtIp.TabIndex = 0;
+            this.txtIp.Text = "192.168.1.10";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(201, 39);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(73, 20);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "1032";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(315, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(43, 85);
+            this.txtSend.Multiline = true;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(589, 119);
+            this.txtSend.TabIndex = 3;
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(43, 224);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(589, 93);
+            this.txtMsg.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(479, 358);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 406);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientSide
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Socket socketSend;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //建立一个负责通讯的Socket
+                socketSend = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                IPAddress ip = IPAddress.Parse(txtIp.Text);
+
+                IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32(txtPort.Text));
+
+                //获得要连接的远程服务端的IP地址和端口号
+                socketSend.Connect(point);
+
+                ShowMessage("Successfully connected!!");
+
+                Thread th = new Thread(Recieve);
+                th.Start(socketSend);
+                th.IsBackground = true;
+            }
+            catch
+            {
+
+            }
+        }
+
+        public void ShowMessage(string str)
+        {
+            txtSend.AppendText(str + "\r\n");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] buffer=Encoding.UTF8.GetBytes(txtSend.Text.Trim());
+            socketSend.Send(buffer);
+        }
+
+        public void Recieve()
+        {
+            try
+            {
+                while (true)
+                {
+                    byte[] buffer = new byte[1024 * 1024 * 5];
+                    int r = socketSend.Receive(buffer);
+
+                    if (r == 0)
+                    {
+                        break;
+                    }
+                    string s = Encoding.UTF8.GetString(buffer, 0, r);
+                    ShowMessage(socketSend.RemoteEndPoint + ":" + s);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+    }
+}
